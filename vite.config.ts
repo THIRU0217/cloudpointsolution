8e34f071d9a6879755b6a1b60242f4020436d686
Thiru <thirukumarant143@gmail.com>
2026-02-13 21:29:05 +0530

    fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cloudpointsolution/',
-
+  base: '/',   // ROOT for custom domain
   plugins: [react()],
-  
   build: {
     outDir: 'dist',
     sourcemap: false
