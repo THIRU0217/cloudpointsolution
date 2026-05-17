4d05983a03b4f0b2eb763da62f137b6e2dd8fb2d
Thiru <thirukumarant143@gmail.com>
2026-05-17 15:53:17 +0530

    Stabilize gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',   // ROOT for custom domain
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  base: '/',
 })
